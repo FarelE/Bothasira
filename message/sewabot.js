d76ae40211f49278af78f273485dfbc07805c75d
kintolgede60 <kintolgede60@gmail.com>
2023-03-14 13:21:33 +0700

    work

---
+++ message/sewabot.js
@@ -0,0 +1,53 @@
+
+
+export const dada = (prefix, pushname, ucapanWaktu) => {
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001) 
+return `${ucapanWaktu} kak
+Berikut adalah list harga untuk
+
+𝗦𝗘𝗪𝗔 𝗕𝗢𝗧 :
+
+╭─❒ 「  *𝘚𝘦𝘸𝘢 𝘎𝘳𝘰𝘶𝘱* 」
+│ Harga!
+├ Pengguna baru Rp. 5k
+├ Masa aktif 15 Hari
+├ Free Givesaldo + Givelimit
+├ Free Premium Semua Admin
+╰─────────⁛⸙
+`+readmore+`
+╭─❒ 「  *𝘚𝘦𝘸𝘢 𝘎𝘳𝘰𝘶𝘱 2* 」
+│ Harga!
+├ Pengguna baru Rp. 10k
+├ Masa aktif 1 Bulan
+├ Perpanjang Rp. 5k
+├ Free Givesaldo + Givelimit
+├ Free Premium Semua Admin
+╰─────────⁛⸙
+
+𝗡𝗢𝗧𝗘 : 
+
+Bot on 24 jam tapi kadang juga mati
+Atau lgi ada error perbaikan Bug .
+`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ responstik/autostiker.js
@@ -0,0 +1,45 @@
+//AUTO RESPON STIKER
+//JANGAN DI UBAH YEE
+
+export const stikSpam = [
+"./temp/stick/jangan spam dong.webp",
+"./temp/stick/bentar dulu kak.webp",
+]
+
+export const stikOtw = [
+"./temp/stick/oke tunggu bentar.webp",
+]
+
+export const stikThanks = [
+"./temp/stick/sama sama kak.webp",
+]
+
+export const stikAdmin = [
+"./temp/stick/hanya admin kak.webp",
+]
+
+export const stikSalam = [
+""
+]
+
+export const stikOwner = [
+""
+]
+
+export const stikToxic = [
+""
+]
+
+export const stikNot = [
+"./temp/stick/hadeuhh.webp",
+"./temp/stick/maap kak ga ada.webp",
+"./temp/stick/ga ada dalam menu.webp"
+]
+
+
+
+
+
+
+
+
